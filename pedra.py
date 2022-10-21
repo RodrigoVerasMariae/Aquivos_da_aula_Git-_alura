3f1590a5369e963192fa87ae5c740f99d642adc4
Rodrigo Veras <112950516+RodrigoVerasMariae@users.noreply.github.com>
2022-10-21 19:21:23 -0300

    Alterando codigo

--- pedra.py
+++ pedra.py
@@ -2,26 +2,34 @@ from random import randint
 from time import sleep
 
 maquina = randint(0,2)
+
 print('*'*10)
 print('BORA JOGAR!')
 print('*'*10)
+
 time = sleep(1)
+
 print('''
 [1] Pedra
 [2] Papel
 [3] Tesoura
 ''')
+
 time = sleep(1)
+
 print('>'*17)
 print('ESCOLHA SUA JOGADA')
 print('>'*17)
+
 jogador = int(input('Escolha sua jogada: '))
+
 print('Pedra...')
 tempo = sleep(1)
 print('Papel...')
 tempo = sleep(1)
 print('Tesoura!')
 tempo = sleep(1)
+
 if maquina == 0:
     if jogador == 1:
         print('A maquina escolheu pedra e voce pedra também, empate!')
@@ -29,6 +37,7 @@ if maquina == 0:
         print('A maquina escolheu pedra e voce papel, voce ganhou!')
     if jogador == 3:
         print('A maquina escolheuu pedra e voce tesoura, a maquina ganhou!')
+
 elif maquina == 1:
     if jogador == 1:
         print('A maquina escolheu papel e voce pedra, a maquina ganhou!')
@@ -36,6 +45,7 @@ elif maquina == 1:
         print('A maquina escolheu papel e voce papel, deu empate!')
     if jogador == 3:
         print('A maquina escolheu papel e voce tesoura, voce ganhou!')
+        
 elif maquina == 2:
     if jogador == 1:
         print('A maquina escolheu tesoura e voce pedra, voce ganhou!')
